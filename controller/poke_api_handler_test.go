8a444068dc796ed3fa28b93538b6ab8ed50faa58
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-04-19 22:37:04 -0500

    Add tests for errors

--- controller/poke_api_handler_test.go
+++ controller/poke_api_handler_test.go
@@ -3,9 +3,12 @@ package controller
 import (
 	"catching-pokemons/models"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
+	"net/http"
 	"testing"
 
+	"github.com/jarcoal/httpmock"
 	"github.com/stretchr/testify/require"
 )
 
@@ -24,4 +27,62 @@ func TestGetPokemonFromPokeApiSuccess(t *testing.T) {
 	c.NoError(err)
 
 	c.Equal(pokemon, expected)
+}
+
+func TestGetPokemonFromPokeApiSuccessWithMocks(t *testing.T) {
+	c := require.New(t)
+
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	id := "bulbasaur"
+
+	body, err := ioutil.ReadFile("samples/poke_api_response.json")
+	c.NoError(err)
+
+	request := fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%s", id)
+
+	httpmock.RegisterResponder(http.MethodGet, request, httpmock.NewBytesResponder(200, body))
+
+	pokemon, err := GetPokemonFromPokeApi(id)
+	c.NoError(err)
+
+	var expectedResponse models.PokeApiPokemonResponse
+	json.Unmarshal([]byte(body), &expectedResponse)
+
+	c.Equal(expectedResponse, pokemon)
+}
+
+func TestGetPokemonFromPokeApiServerInternalError(t *testing.T) {
+	c := require.New(t)
+
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	id := "bulbasaur"
+
+	request := fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%s", id)
+
+	httpmock.RegisterResponder(http.MethodGet, request, httpmock.NewStringResponder(500, ""))
+
+	_, err := GetPokemonFromPokeApi(id)
+	c.NotNil(err)
+	c.EqualError(err, ErrPokeApiFailure.Error())
+}
+
+func TestGetPokemonFromPokeApiNotFound(t *testing.T) {
+	c := require.New(t)
+
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	id := "najsndkjnaksjdnkjan"
+
+	request := fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%s", id)
+
+	httpmock.RegisterResponder(http.MethodGet, request, httpmock.NewStringResponder(404, ""))
+
+	_, err := GetPokemonFromPokeApi(id)
+	c.NotNil(err)
+	c.EqualError(err, ErrPokemonNotFound.Error())
 }
